12a7f247b2eaccda5f2f242be0321bc17a5bf8a9
Mohammad Aarman <inn.mdaarman@gmail.com>
2025-08-26 23:03:20 +0545

    Updated

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,18 @@
+<?php 
+
+use app\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+// class PostController extends Controller
+// {
+//     public function index()
+//     {   
+//         $posts = Auth::user()->posts()->paginate(10);
+//         return view("        ", compact(""));
+
+//     }
+//     public function create()
+//     {
+//         return view("");
+//     }
+// }
--- routes/web.php
+++ routes/web.php
@@ -1,63 +1,95 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
-});
 
+});
 
 Route::get('/about', function () {
-    return view('about');
+    return 'Hey! Whose This in about page?';
 });
 
 
-// http verbs
-// get, post, put/patch, delete, any, match 
 
-// Route::get('/user', function () {
-//     return 'User Access';
-// });
+// Named Paramater Route
+// Route::get('user/{id}', function ($id) {
+//     return 'User '.$id;
+// })->name('user.show');
 
-// Route::post('/user', function (){
-//     return 'User Created';
-// });
-
-// Route::match(['get', 'post'], '/user', function () {
-//     return 'User Access';
-// });
-
-// Route::put('/user/{id}', function ($id) {
-//     return "User with ID {$id} updated";
-// });
-
-// Route::patch('/user/{id}', function ($id) {
-//     return "User with ID {$id} partially updated";
-// });
-
-// Route::delete('/user/{id}', function ($id) {
-//     return "User with ID {$id} deleted";
-// });
-
-// Route::any('/anything', function() {
-//     return 'Matched any HTTP method';
-// });
-
-
-
-// // Route parameter
-// Route::get('/user/{id}', function ($id) {
-//     return "User with ID {$id}";
-// });
-
-
-// // optional Route parameter
-// Route::get('/user/{id?}', function ($id = null) {
-//     return "User with ID {$id}";
-// });
-
-
-//multiple parameter
-Route::get('/user/{id}/{name}', function ($id, $name) {
-    return "User with ID {$id} and Name {$name}";
+// Grouping Routes
+Route::prefix('admin')->group(function () {
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });    
+    Route::get('/posts', function () {
+        return 'Admin Posts';
+    }); 
 });
+
+// constraints
+
+Route::get('/user/{ids}', function ($ids){
+    return 'User '.$ids;
+})->where('ids','[0-9]+'); // only numbers
+
+Route::get('/user/{slug}', function ($slug){
+    return 'Post with slug '.$slug;
+})->where('slug','[A-Za-z0-9-_]+'); // only letters
+
+
+// // middleware
+// Route::middleware('/auth', function () {
+//     Route::get('/profile', function () {
+//         return 'Profile Page';
+//     });
+//     Route::get('/settings', function () {
+//         return 'Settings Page';
+//     });
+// });
+
+// Combined Example
+Route::prefix('admin')->middleware('/auth')->group(function () {        // middleware(['auth])
+    Route::get('/home', function () {
+        return 'Admin Home Page';
+    });
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard Page';
+    });
+});
+
+// Fallback Route
+Route::fallback(function () {
+    // return '404 Not Found. Go back to <a href="/">Home</a>';
+    return view('errors.404');
+});
+
+// Route::get('/post', [PostController::class, 'index'])->name('post.index');
+// Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
+// Route::post('/post', [PostController::class, 'store'])->name('post.store');
+// Route::get('/post/{id}', [PostController::class, 'show'])->name('post.show');
+// Route::get('/post/{id}/edit', [PostController::class, 'edit'])->name('post.edit');
+// Route::put('/post/{id}', [PostController::class, 'update'])->name('post.update');
+// Route::delete('/post/{id}', [PostController::class, 'destroy'])->name('post.destroy');
+
+// Route::resource('post', PostController::class);
+// Route::resource('post', PostController::class)->only([
+//     'index', 'show', 'create', 'store', 'edit'
+// ]);
+
+// Route::resource('post', PostController::class)->except([
+//     'update', 'destroy'
+// ]);
+
+// Route::controller(PostController::class)->group(function () {
+//     Route::get('/post', 'index')->name('post.index');
+//     Route::get('/post/create', 'create')->name('post.create');
+//     Route::post('/post', 'store')->name('post.store');
+//     Route::get('/post/{id}', 'show')->name('post.show');
+//     Route::get('/post/{id}/edit', 'edit')->name('post.edit');
+//     Route::put('/post/{id}', 'update')->name('post.update');
+//     Route::delete('/post/{id}', 'destroy')->name('post.destroy');
+// });
+
